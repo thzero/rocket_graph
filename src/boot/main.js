864f07ac1803ca92f25d93fa772d35b70e7554b3
thzero <corvette@thzero.com>
2021-11-24 11:09:11 -0600

    moved electron centric store into services

--- src/boot/main.js
+++ src/boot/main.js
@@ -7,4 +7,10 @@ import AppUtility from '../utility';
 export default boot(async ({ app, router, store }) => {
 	AppUtility.$router = router;
 	AppUtility.$store = store;
+	console.log(app);
+	console.log(app);
+	console.log(app);
+	console.log(app);
+	console.log(app);
+	console.log(app);
 });
